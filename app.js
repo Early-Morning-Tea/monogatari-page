4f20c5823ad7d99c19d82332fcf104c2718a1325
charlestian23 <charles.tian23@gmail.com>
2022-08-04 20:17:32 -0400

    Buggy implementation that doesn't really work but still want to save it

--- app.js
+++ app.js
@@ -11,10 +11,13 @@ function toggle() {
 
 const images = document.querySelectorAll('#second img');
 const buttons = document.querySelectorAll('.button');
+const links = document.querySelectorAll(".links");
 images.forEach(image => image.addEventListener('mouseover', hover));
 images.forEach(image => image.addEventListener('mouseout', stopHover));
 buttons.forEach(button => button.addEventListener('mouseover', hover));
 buttons.forEach(button => button.addEventListener('mouseout', stopHover));
+console.log(links);
+links.forEach(link => link.addEventListener('click', setToVisited));
 
 function hover(e) {
     this.classList.add('hovering');
@@ -22,4 +25,10 @@ function hover(e) {
 
 function stopHover(e) {
     this.classList.remove('hovering');
+}
+
+function setToVisited(e) {
+    console.log(this);
+    links.forEach(link => link.classList.remove("selected"));
+    this.classList.add("selected");
 }
